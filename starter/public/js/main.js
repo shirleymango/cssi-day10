01d9be10555ef9a5d5ff1262bfac10af65be6d1f
Shirley Zhu <shirleyzhu@gmail.com>
2021-07-23 17:00:49 +0000

    mini project

--- starter/public/js/main.js
+++ starter/public/js/main.js
@@ -1,1 +1,18 @@
 console.log("Script Running!");
+
+document.querySelector("#search").addEventListener("keypress", (e) => { 
+    if (e.key == "Enter"){ 
+        const searchYear = search.value;
+        const locationList = firebase.database().ref();
+        console.log(searchYear);
+        locationList.on('value', (snapshot) => {
+            const data = snapshot.val();
+            for(let key in data) {
+                if(searchYear == data[key].Year) {
+                    document.querySelector("#cityName").innerHTML = data[key].City;
+                    document.querySelector("#countryName").innerHTML = data[key].Country;
+                }
+            }
+        })
+    } 
+});
